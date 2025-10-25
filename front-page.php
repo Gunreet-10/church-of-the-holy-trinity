72cee030603af587d60ea95c8546ee5ae3c98775
Gunreet-10 <gunreetkaur1010@gmail.com>
2025-10-25 00:51:41 -0600

    added test content in front-page.php

--- front-page.php
+++ front-page.php
@@ -1,4 +1,34 @@
 <?php
-get_header();
-get_template_part('home'); // This loads home.php
-get_footer();
+
+/**
+ * Template Name: Front Page
+ * Description: Custom static homepage for Church of the Holy Trinity
+ */
+
+get_header(); ?>
+
+<main id="main-content" class="site-main">
+    <section class="hero">
+        <div class="container">
+            <h1>Welcome to Church of the Holy Trinity</h1>
+            <p>We’re glad you’re here. This is our new website under development.</p>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="container">
+            <?php
+            // Display page content if it exists
+            if (have_posts()) :
+                while (have_posts()) : the_post();
+                    the_content();
+                endwhile;
+            else :
+                echo '<p>Content coming soon.</p>';
+            endif;
+            ?>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
